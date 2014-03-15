061d0d54451ba96aba54ece7808260ce3ad06795
orkaner <plastikt@hotmail.com>
2014-03-15 16:09:27 -0700

    search feature added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Contacts::Application.config.secret_token = '25456ca0d955fe69eee3a7dfc7645722ad9157090713b182208b6c859c10e0d1e6b730c09bf3586e27f91a34afab01e2751ff99ca407caaad38ae060a993ea1d'
